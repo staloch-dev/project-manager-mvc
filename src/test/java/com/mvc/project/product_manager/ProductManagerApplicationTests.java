dc98ab8688641b8cd07c0bce927334f257d71e75
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2025-10-28 20:39:31 -0300

    feat: Initial project setup with all required dependencies and Spring structure

---
+++ src/test/java/com/mvc/project/product_manager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.project.product_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
